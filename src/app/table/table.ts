8c8ebfd08ce4a2b749fe0a338e9c555ff1a91adf
Raydstore <raydstore@gmail.com>
2018-07-09 09:19:57 +0200

    new win installation and manual copy of the project

--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,5 +1,10 @@
 export type TFunctionName = <T, K>(item: T, arg: K) => any;
-
+export enum Mode {insert = 0, update = 1, delete = 2}
+export interface EventAccidentArgs {
+    item: Accident;
+    mode: Mode;
+    dialogVisible: boolean;
+}
 
 export interface Activity {
     id?: number;
